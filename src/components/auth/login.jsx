1b702fb478bea1315371a6a23a32bd5e47d9483c
Maia <maia@Citadel.local>
2019-03-26 15:34:03 -0300

    Initial Commit

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,11 +1,52 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
-  state = {}
+  state = {
+    email: '',
+    password: '',
+    errors: ''
+  }
+
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    const credentials = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    console.log(credentials)
+  }
+
   render() {
     return (
       <div>
         <h1>Login</h1>
+
+        <form onSubmit={this.handleSubmit} noValidate>
+          <div className="form-element">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={this.handleInput}
+            />
+          </div>
+          <div className="form-element">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleInput}
+            />
+          </div>
+          <button>Submit</button>
+        </form>
       </div>
     )
   }
--- src/components/auth/register.jsx
+++ src/components/auth/register.jsx
@@ -1,24 +1,91 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Register extends Component {
-  state = {}
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+    errors: {}
+  }
+
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    }
+    axios
+      .post('/users', newUser)
+      .then(result => {
+        console.log(result.data)
+      })
+      // SET THE ERRORS RESPONSE TO THE STATE
+      .catch(err => this.setState({ errors: err.response.data }))
+  }
+
   render() {
+    // GET THE ERRORS OBJECT FROM STATE
+    const { errors } = this.state
+
     return (
       <div>
         <h1>Register</h1>
 
-        <form action="">
+        <form onSubmit={this.handleSubmit} noValidate>
           <div className="form-element">
-            <input type="text" name="name" id="name" placeholder="Name" />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Name"
+              value={this.state.name}
+              onChange={this.handleInput}
+              className={errors.name ? 'error' : null}
+            />
+            {errors.name && <small>{errors.name}</small>}
           </div>
           <div className="form-element">
-            <input type="email" name="email" id="email" placeholder="Email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={this.handleInput}
+              className={errors.email ? 'error' : null}
+            />
+            {errors.email && <small>{errors.email}</small>}
           </div>
           <div className="form-element">
-            <input type="password" name="password" id="password" placeholder="Password" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleInput}
+              className={errors.password ? 'error' : null}
+            />
+            {errors.password && <small>{errors.password}</small>}
           </div>
           <div className="form-element">
-            <input type="password" name="password2" id="password2" placeholder="Confirm Password" />
+            <input
+              type="password"
+              name="password2"
+              id="password2"
+              placeholder="Confirm Password"
+              value={this.state.password2}
+              onChange={this.handleInput}
+              className={errors.password2 ? 'error' : null}
+            />
+            {errors.password2 && <small>{errors.password2}</small>}
           </div>
           <button>Submit</button>
         </form>
