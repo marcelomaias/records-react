0144ff8ae16e9e72a73dcde4d2ace3a0467f7c13
Maia <maia@Citadel.local>
2019-04-03 02:47:27 -0300

    Added correct links

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -4,8 +4,23 @@ const Footer = () => {
   return (
     <footer>
       <div>
-        Made with <a href="http://reactjs.org">React</a>, <a href="/">Redux</a>, <a href="/">Express</a>,{' '}
-        <a href="/">MongoDB</a>.
+        Made with{' '}
+        <a href="http://reactjs.org" target="_blank" rel="noopener noreferrer">
+          React
+        </a>
+        ,{' '}
+        <a href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
+          Redux
+        </a>
+        ,{' '}
+        <a href="https://expressjs.com/" target="_blank" rel="noopener noreferrer">
+          Express
+        </a>
+        ,{' '}
+        <a href="https://www.mongodb.com/" target="_blank" rel="noopener noreferrer">
+          MongoDB
+        </a>
+        .
       </div>
     </footer>
   )
